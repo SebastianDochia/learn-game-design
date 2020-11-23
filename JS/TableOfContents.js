14ccdd3a3888cb7fb0ed7e1d1806870a544261fd
SebastianDochia <sebastiam.dochia@gmail.com>
2020-11-23 21:00:30 +0200

    update

---
+++ JS/TableOfContents.js
@@ -0,0 +1,9 @@
+makeContentsVisible = () => {
+    if(document.getElementById('CoT').style.display== "block") {
+        document.getElementById('CoT').style.display= "none";
+    }else {
+        document.getElementById('CoT').style.display= "block";
+    }
+    
+    //document.getElementById('Toc').classList.add('animate');
+}
